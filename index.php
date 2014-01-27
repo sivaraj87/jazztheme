235ac046077e842cb16b8bd5e9b2b0c2f5bda261
Angela J. Holden <angelajholden@gmail.com>
2014-01-26 21:38:50 -0800

    Contact form is working, adjustment to sent message CSS.

--- index.php
+++ index.php
@@ -77,52 +77,67 @@
               <li>If you need content removed or edited, can you tell us why? Can you provide a source for the requested edits?</li>
             </ol>
             <div class="contactForm">
-              <?php
+            <?php
               function spamcheck($field)
-                {
-                //filter_var() sanitizes the e-mail
-                //address using FILTER_SANITIZE_EMAIL
-                $field=filter_var($field, FILTER_SANITIZE_EMAIL);
+              {
+              //filter_var() sanitizes the e-mail
+              //address using FILTER_SANITIZE_EMAIL
+              $field=filter_var($field, FILTER_SANITIZE_EMAIL);
 
-                //filter_var() validates the e-mail
-                //address using FILTER_VALIDATE_EMAIL
-                if(filter_var($field, FILTER_VALIDATE_EMAIL))
-                  {
-                  return TRUE;
-                  }
-                else
-                  {
-                  return FALSE;
-                  }
+              //filter_var() validates the e-mail
+              //address using FILTER_VALIDATE_EMAIL
+              if(filter_var($field, FILTER_VALIDATE_EMAIL))
+                {
+                return TRUE;
                 }
+              else
+                {
+                return FALSE;
+                }
+              }
 
               if (isset($_REQUEST['email']))
-                {//if "email" is filled out, proceed
+              {//if "email" is filled out, proceed
 
-                //check if the email address is invalid
-                $mailcheck = spamcheck($_REQUEST['email']);
-                if ($mailcheck==FALSE)
-                  {
-                  echo "<h2 style='text-align:center;color:white'>Oops! Something went wrong. Try again.</h2>";
+              //check if the email address is invalid
+              $mailcheck = spamcheck($_REQUEST['email']);
+              if ($mailcheck==FALSE)
+                {
+                echo "<h2 style='text-align:center;color:white'>Oops! Something went wrong. Try again.</h2>
+                      <form action='index.php' method='post'>
+                      <div><input type='text' name='name' placeholder='Name' /></div>
+                      <div><input type='email' name='email' placeholder='Email' /></div>
+                      <div><textarea cols='40' rows='8' name='message' placeholder='Message'></textarea></div>
+                      <div class='submit'><input type='submit' value='Send' /></div>
+                      </form>"
+                    ;
                   }
-                else
-                  {//send email
-                  $name = $_REQUEST['name'] ;
-                  $email = $_REQUEST['email'] ;
-                  $message = $_REQUEST['message'] ;
-                  mail("contact@wikipediageeks.com", "Email from $name",
-                  $message, "From: $email" );
-                  echo "<h2 style='text-align:center;color:white'>Thank you! Your message has been sent.</h2>";
+              else
+                {//send email
+                $name = $_REQUEST['name'] ;
+                $email = $_REQUEST['email'] ;
+                $message = $_REQUEST['message'] ;
+                mail("contact@wikipediageeks.com", "Message from $name",
+                $message, "From: $email" );
+                echo "<h2 style='text-align:center;color:white'>Thank you! Your message has been sent.</h2>
+                      <form action='index.php' method='post'>
+                      <div><input type='text' name='name' placeholder='Name' /></div>
+                      <div><input type='email' name='email' placeholder='Email' /></div>
+                      <div><textarea cols='40' rows='8' name='message' placeholder='Message'></textarea></div>
+                      <div class='submit'><input type='submit' value='Send' /></div>
+                      </form>"
+                    ;
                   }
                 }
               else
-                {//if "email" is not filled out, display the form
-                echo "<form action=php echo htmlspecialchars('index.php'); method='post'>
-                        <input class='contact' type='text' name='name' placeholder='Name' />
-                        <input class='contact' type='email' name='email' placeholder='Email' />
-                        <textarea class='contact' cols='40' rows='8' name='message' placeholder='Message'></textarea>
-                        <div class='submit'><input type='submit' value='Send' /></div>
-                      </form>";
+              {//if "email" is not filled out, display the form
+              echo "<form action='index.php' method='post'>
+                    <div><input type='text' name='name' placeholder='Name' /></div>
+                    <div><input type='email' name='email' placeholder='Email' /></div>
+                    <div><textarea cols='40' rows='8' name='message' placeholder='Message'></textarea></div>
+                    <div class='submit'><input type='submit' value='Send' /></div>
+                    </form>"
+                  ;
                 }
               ?>
             </div>
