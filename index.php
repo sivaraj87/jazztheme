45a5e5d2cb59c957de33d26ddf6ed67b1e3a07cb
Angela J. Holden <angelajholden@gmail.com>
2014-01-26 21:20:52 -0800

    Added PHP to power contact form.

--- index.php
+++ index.php
@@ -11,12 +11,13 @@
     <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
   </head>
-    <body>
+    <body id="home">
       <nav>
         <ul>
-          <li><a href="#">Home</a></li>
+          <li><a href="#home">Home</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#price">Price</a></li>
+          <li><a href="#examples">Examples</a></li>
           <li><a href="#contact">Contact</a></li>
           <li><a href="mailto:contact@wikipediageeks.com">contact@wikipediageeks.com</a></li>
           <li><a href="tel:+16197191069">(619) 719-1069</a></li>
@@ -42,8 +43,29 @@
             <p>We’ve been contributing to <a href="#">Wikipedia</a> for a long time. We do it because we believe the internet is a better place when information is shared freely and consumed critically. We understand Wikipedia’s policy on tone and neutrality, and work hard to make contributions that respect those rules. We’ve also spent hundreds of hours learning the Wikipedia markup language and how to format and categorize article content appropriately.</p>
           </article>
           <article id="price" class="third">
-            <h2>Our price is $500 per article.</h2>
-            <p class="ancillary">We warranty our work for 6 months.<br>If we can't sustain your page or edits for 6 months, we'll give you a full refund.</p>
+            <h2>$500 per Article</h2>
+            <p class="ancillary">We guaranty our work for 6 months.<br>If we can't sustain your page or edits for 6 months, we'll give you a full refund.</p>
+            <p>We know what you’re thinking… “Why can’t I just do this myself?” You certainly can, and we encourage you to try. But did you know there are thousands of volunteer Wikipedia Administrators looking for pages that contain marketing and promotional language? Even the slightest misstep in your writing can be cause for immediate deletion.</p>
+          </article>
+          <article id="examples" class="three-and-a-half">
+            <h2>Examples</h2>
+            <p class="ancillary">These screenshots illustrate what you can acheive with a Wikipedia page.</p>
+            <hr>
+            <figure class="screenShots">
+              <figcaption><p>Unordered & Ordered Lists</p></figcaption>
+              <a class="fancybox" href="images/WikipediaArticle-500Words.png"><img src="images/WikipediaArticle-500Words.png"></a>
+              <p><a href="http://en.wikipedia.org/wiki/Anna_Maria_Mozart" target="_blank"> View Anna Maria Mozart</a></p>
+            </figure>
+            <figure class="screenShots">
+              <figcaption><p>Sortable Tables</p></figcaption>
+              <a class="fancybox" href="images/Kochel-catalogue.png"><img src="images/Kochel-catalogue.png"></a>
+              <p><a href="http://en.wikipedia.org/wiki/K%C3%B6chel_catalogue" target="_blank">View the Köchel catalogue</a></p>
+            </figure>
+            <figure class="screenShots">
+              <figcaption><p>Track Lists & Album Covers</p></figcaption>
+              <a class="fancybox" href="images/Mozarts-House.png"><img src="images/Mozarts-House.png"></a>
+              <p><a href="http://en.wikipedia.org/wiki/Mozart's_House" target="_blank">View Mozart's House</a></p>
+            </figure>
           </article>
           <article id="contact" class="fourth">
             <h2>Contact</h2>
@@ -55,14 +77,54 @@
               <li>If you need content removed or edited, can you tell us why? Can you provide a source for the requested edits?</li>
             </ol>
             <div class="contactForm">
-              <form action='' method='post'>
-                <div><label for="name"><input type="text" name="message_name" placeholder="Name" value=""></label></div>
-                <div><label for="message_email"><input type="text" name="message_email" placeholder="Email" value=""><label></div>
-                <div><label for="message_text"><textarea cols='40' rows='8' type="text" name="message_text" placeholder="Message" ></textarea><label></div>
-                <div><label for="message_human"><input type="text" style="width:60px;" name="message_human"> + 3 = 5<label></div>
-                <input type="hidden" name="submitted" value="1">
-                <div class='submit'><input type='submit' value='Send' /></div>
-              </form>
+              <?php
+              function spamcheck($field)
+                {
+                //filter_var() sanitizes the e-mail
+                //address using FILTER_SANITIZE_EMAIL
+                $field=filter_var($field, FILTER_SANITIZE_EMAIL);
+
+                //filter_var() validates the e-mail
+                //address using FILTER_VALIDATE_EMAIL
+                if(filter_var($field, FILTER_VALIDATE_EMAIL))
+                  {
+                  return TRUE;
+                  }
+                else
+                  {
+                  return FALSE;
+                  }
+                }
+
+              if (isset($_REQUEST['email']))
+                {//if "email" is filled out, proceed
+
+                //check if the email address is invalid
+                $mailcheck = spamcheck($_REQUEST['email']);
+                if ($mailcheck==FALSE)
+                  {
+                  echo "<h2 style='text-align:center;color:white'>Oops! Something went wrong. Try again.</h2>";
+                  }
+                else
+                  {//send email
+                  $name = $_REQUEST['name'] ;
+                  $email = $_REQUEST['email'] ;
+                  $message = $_REQUEST['message'] ;
+                  mail("contact@wikipediageeks.com", "Email from $name",
+                  $message, "From: $email" );
+                  echo "<h2 style='text-align:center;color:white'>Thank you! Your message has been sent.</h2>";
+                  }
+                }
+              else
+                {//if "email" is not filled out, display the form
+                echo "<form action=php echo htmlspecialchars('index.php'); method='post'>
+                        <input class='contact' type='text' name='name' placeholder='Name' />
+                        <input class='contact' type='email' name='email' placeholder='Email' />
+                        <textarea class='contact' cols='40' rows='8' name='message' placeholder='Message'></textarea>
+                        <div class='submit'><input type='submit' value='Send' /></div>
+                      </form>";
+                }
+              ?>
             </div>
           </article>
         </section>
@@ -71,8 +133,9 @@
       </footer>
 
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-	  <script src="js/jquery.fancybox.min.js"></script>
-	  <script src="js/global.min.js"></script>
+    <script src="js/easing.min.js"></script>
+    <script src="js/fancybox.min.js"></script>
+    <script src="js/global.min.js"></script>
 
     </body>
 </html>
--- js/global.js
+++ js/global.js
@@ -12,5 +12,15 @@ $(document).ready(function() {
     openEffect  : 'elastic',
     closeEffect : 'elastic'
   });
+
+// Easing
+  $('nav a').bind('click',function(event){
+    var $anchor = $(this);
+
+    $('html, body').stop().animate({
+      scrollTop: $($anchor.attr('href')).offset().top
+    }, 1500,'easeInOutExpo');
+    event.preventDefault();
+  });
   
 });
